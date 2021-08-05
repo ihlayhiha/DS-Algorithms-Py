5b07fc07ad33b89df3537c58cff319b4e973b280
Teja Linux <raviteja.yella@gmail.com>
2021-08-05 17:02:40 +0530

    changing stack memory for max recursive depth

---
+++ Recursion/stack_memory.py
@@ -0,0 +1,15 @@
+# we can increase the stack memory to deal with plausible recursion depth error
+
+import sys
+sys.setrecursionlimit(10000)    # basically has a recursive depth of 10000, i.e program limits adding stack memory at 10000 recursive steps
+
+
+def factorial(n):
+    """calculating the 'n' th factorial using recursion"""
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(10))
