c046b315eb06f519d4dd588dd97adb7ad3de8b01
Teja Linux <raviteja.yella@gmail.com>
2021-08-05 15:56:38 +0530

    adding random recursive fun

--- delete folder/delete2.py
+++ delete folder/delete2.py
@@ -1,1 +1,12 @@
 # trying to check if this will work too
+
+
+def recursiveMethod(n):
+    if n < 1:
+        print("n is less than 1")
+    else:
+        recursiveMethod(n - 1)
+        print(n)
+
+
+recursiveMethod(4)
