b9f60cc6c915ecaf6b2729b8f0b92c47ef34595c
Teja Linux <raviteja.yella@gmail.com>
2021-08-05 17:13:35 +0530

    same

--- Recursion/stack_memory.py
+++ Recursion/stack_memory.py
@@ -6,11 +6,11 @@ sys.setrecursionlimit(10000)    # basically has a recursive depth of 10000, i.e 
 
 def factorial(n):
     """calculating the 'n' th factorial using recursion"""
-    assert(n >= 0), "n should be > 0, Factorial only exits for non-negative integers"
+    assert n >= 0 and int(n) == n, "Factorial only exits for non-negative integers, 'n' has to be non-negative integer"
     if n in [0, 1]:
         return 1
     else:
         return n * factorial(n - 1)
 
 
-print(factorial(10))
+print(factorial(1.5))
