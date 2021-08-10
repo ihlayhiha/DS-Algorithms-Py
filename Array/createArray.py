de455e24f562b5c24c32f5f94e3d416b3ec3ea86
Teja Linux <raviteja.yella@gmail.com>
2021-08-10 18:33:59 +0530

    added insert method

--- Array/createArray.py
+++ Array/createArray.py
@@ -5,3 +5,21 @@ arr2 = array('d', [1.3, 1.5, .8])
 
 
 print(arr1, arr2)
+
+
+# inserting 10 as the 7 th element into arr1
+arr1.insert(6, 10)
+print(arr1)
+# inserting 11 as the first element into arr1
+# the array moves to the right automatically
+arr1.insert(0, 11)
+print(arr1)
+# inserting something into the 2nd index
+# moves everything after 2nd element to the right and inserts there.
+arr1.insert(2, 12)
+print(arr1)
+
+# Time Complexity for inserting is dependent on where u wanna insert and the worst case is O(n)
+print(len(arr1))
+arr1.insert(9, 99)
+print(len(arr1))
